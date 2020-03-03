b9ee28553649dd285e690b9ade24417aee94d084
binding-archive-7591 <yarhumher@hotmail.com>
2020-03-03 23:55:22 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -12,7 +12,7 @@ def fourth_challenge
       :patriarch => {name: "Lord Capulet", age: "50"},
       :matriarch => {name: "Lady Capulet", age: "51"},
       :heroine => {name: "Juliet", age: "15", status: "alive"},
-      :heroine_friends => [
+      :heroine_friends => [{:age=>"30", :attitude=>"confused", :name=>"Steven"}, {:age=>"44", :attitude=>"worried", :name=> "Nurse"
         # {name: "Steven", age: "30", attitude: "confused"}, {name: "Nurse", age: "44", attitude: "worried"}
         #your hashes here!
       ]
