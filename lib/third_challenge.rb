34ed261b8602e05d3aad7cfc0a27534ae03e160a
binding-archive-7591 <yarhumher@hotmail.com>
2020-03-03 23:37:49 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -10,7 +10,7 @@ def third_challenge
         #your key/value pairs here
         },
       :hero => {
-        :name => "", :age => "", :status => ""]
+        :name => "", :age => "", :status => ""
         #your key/value pairs here
         },
       :hero_friends => []
