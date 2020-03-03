26b6b4caa94aad52de927af5adff511e176a299f
binding-archive-7591 <yarhumher@hotmail.com>
2020-03-03 23:44:55 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -17,7 +17,7 @@ def third_challenge
    },
    :capulet => {
       :patriarch => {
-        :name => "", :age => ""
+        :name => "Lord Capulet", :age => "50"
         #your key/value pairs here
         },
       :matriarch => {
