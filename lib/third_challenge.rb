50359b1e177e743cc5dd9a7b169220d6a7041b40
binding-archive-7591 <yarhumher@hotmail.com>
2020-03-03 23:38:47 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -2,7 +2,7 @@ def third_challenge
   epic_tragedy = {
    :montague => {
       :patriarch => {
-        :name => "", :age => ""
+        :name => "Lord Montague", :age => "53"
         #your key/value pairs here
         },
       :matriarch => {
