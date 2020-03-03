e3b55f1c881bee09e74478111e777a0828d794e5
binding-archive-7591 <yarhumher@hotmail.com>
2020-03-03 23:39:27 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -6,7 +6,7 @@ def third_challenge
         #your key/value pairs here
         },
       :matriarch => {
-        :name => "", :age => ""
+        :name => "Lady Montague", :age => "54"
         #your key/value pairs here
         },
       :hero => {
